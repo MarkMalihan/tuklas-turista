6029d9ef779d6549764b673ea3e669d1ec058f21
Mark Angelo Malihan <malihanmarkangelolizano@gmail.com>
2022-08-17 07:29:15 +0800

    added

--- about.js
+++ about.js
@@ -16,3 +16,4 @@ function scrollFunction() {
     document.getElementById("navbar").style.background = "none";
   }
 }
+
--- contact.js
+++ contact.js
@@ -16,3 +16,18 @@ function scrollFunction() {
     document.getElementById("navbar").style.background = "none";
   }
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
