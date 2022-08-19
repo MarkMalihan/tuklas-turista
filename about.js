a2e535575be8d44ae8effaa9b1a55100f5816f2e
Mark Angelo Malihan <malihanmarkangelolizano@gmail.com>
2022-08-19 19:32:24 +0800

    scroll back to top button added

--- about.js
+++ about.js
@@ -5,15 +5,25 @@ toggleButton.addEventListener('click', () => {
   navLinks.classList.toggle('active')
 })
 
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
- 
+
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    
+    mybutton.style.display = "block";
     document.getElementById("navbar").style.background = "#161616";
   } else {
-   
+    mybutton.style.display = "none";
     document.getElementById("navbar").style.background = "none";
   }
 }
 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
