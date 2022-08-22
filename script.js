b14713b64923912f99f1f9c6c459abe7738612ba
Mark Angelo Malihan <malihanmarkangelolizano@gmail.com>
2022-08-22 22:38:14 +0800

    Update

--- script.js
+++ script.js
@@ -26,3 +26,8 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+//weather forecast
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
