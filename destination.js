c7b27ba26c684d3a4e839e37cbf2f9cc995d9496
Mark Angelo Malihan <malihanmarkangelolizano@gmail.com>
2022-08-27 19:22:12 +0800

    content and navbar update

--- destination.js
+++ destination.js
@@ -16,3 +16,30 @@ function scrollFunction() {
     document.getElementById("navbar").style.background = "none";
   }
 }
+
+//slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
