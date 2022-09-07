8d19816921684240a08e100c9e686f93aab5bdca
Mark Angelo Malihan <malihanmarkangelolizano@gmail.com>
2022-09-07 15:46:32 +0800

    added

---
+++ foods/callebarrio.js
@@ -0,0 +1,29 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navLinks = document.getElementsByClassName('nav-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navLinks.classList.toggle('active')
+})
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    document.getElementById("navbar").style.background = "#161616";
+  } else {
+    mybutton.style.display = "none";
+    document.getElementById("navbar").style.background = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
