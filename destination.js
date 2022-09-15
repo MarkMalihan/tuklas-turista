23c52a47aa596e0b52508896f5310b92a5f4f897
Mark Angelo Malihan <malihanmarkangelolizano@gmail.com>
2022-09-15 17:15:51 +0800

    Update

--- destination.js
+++ destination.js
@@ -44,10 +44,12 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+//filter
+
 filterSelection("all")
 function filterSelection(c) {
   var x, i;
-  x = document.getElementsByClassName("filterDiv");
+  x = document.getElementsByClassName("card");
   if (c == "all") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
@@ -82,7 +84,7 @@ function w3RemoveClass(element, name) {
 }
 
 // Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
+var btnContainer = document.getElementById("tags");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
