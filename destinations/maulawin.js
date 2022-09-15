f44487e3551e6619808ed04583f38f9185f3921e
Mark Angelo Malihan <malihanmarkangelolizano@gmail.com>
2022-09-15 17:16:35 +0800

    Ratings and review

--- destinations/maulawin.js
+++ destinations/maulawin.js
@@ -26,3 +26,18 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+//review
+const btn = document.querySelector("button");
+      const post = document.querySelector(".post");
+      const widget = document.querySelector(".star-widget");
+      const editBtn = document.querySelector(".edit");
+      btn.onclick = ()=>{
+        widget.style.display = "none";
+        post.style.display = "block";
+        editBtn.onclick = ()=>{
+          widget.style.display = "block";
+          post.style.display = "none";
+        }
+        return false;
+      }
